6587bd95027c0eccda8a18710bbceb10982d79a5
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-17 19:38:35 +0530

    little changes more to be done later

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,11 @@
+import { NextAuthOptions } from "next-auth";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs"
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+         
+    ]
+}
